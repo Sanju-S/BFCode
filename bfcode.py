cc39d9b2e9c1457a50c8bd79ea38ff109e100ba9
Sanju <sanju@localhost.localdomain>
2020-03-14 19:36:14 -0400

    BFCode.py

---
+++ bfcode.py
@@ -0,0 +1,68 @@
+import sys
+
+def write(i):
+	global file
+	n = ord(i)
+	if n < 11:
+		file.write('>'+'+'*n+'.')
+	else:
+		if n % 10 < 5:
+			file.write('>'+'+'*10+'[>'+'+'*(n//10)+'<-]>'+'+'*(n%10)+'.')
+		else:
+			file.write('>'+'+'*10+'[>'+'+'*(n//10)+'+'+'<-]>'+'-'*(10-(n%10))+'.')
+
+def prnt():
+	global file
+	file.write('.')
+
+def nl():
+	global file
+	file.write('>++[>+++++<-]>.')
+
+def prnt(c):
+	global var, var_hold
+	l = []
+	for k in c[1:]:
+		if k.startswith('$'):
+			if k[1:] in var:
+				s = int(var[k[1:]].split(':')[0])
+				e = int(var[k[1:]].split(':')[1])
+				st = ''.join(var_hold[s:e])
+				l.append(st)
+			else:
+				print("Error: var not found")
+		else:
+			l.append(k)
+	l = ' '.join(l)
+	for i in l:
+		code.append(i)
+		write(i)
+	nl()
+
+code = []
+var_hold = []
+var = {}
+inp = {}
+
+file = open('/home/sanju/code/bf/test.bf', 'w')
+
+while True:
+	c = input("-> ").split()
+	if len(c) == 0:
+		continue
+	elif c[0] == ':x':
+		file.close()
+		sys.exit(0)
+	elif c[0] == 'print':
+		if len(c) > 1:
+			prnt(c)
+		else:
+			nl()
+	elif c[1] == '=':
+		if len(c) < 2:
+			print("Error")
+		else:
+			pp = len(var_hold)
+			for j in ' '.join(c[2:]):
+				var_hold.append(j)
+			var[c[0]] = str(pp)+':'+str(len(var_hold))
